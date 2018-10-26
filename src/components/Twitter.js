c836dc21916dfaafded7bb0b5ad866fb60e7148b
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-26 08:55:32 -0700

    adding Twitter widget with live twitter feed

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 const Twitter = props => {
   return (
     <div className="twitter-container">
-      <p>twitter</p>
+      <div className="main-sub-header">
+        <h3 className="main-sub-header-text">TWITTER</h3>
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="sdschools"
+        options={{height: 400}}
+      />
     </div>
   );
 }
