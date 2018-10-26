8b0ee746ec074585a557a2c3c87612dd8ca7d570
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-26 08:49:02 -0700

    Adding links to staff links

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const QuickLinks = props => {
+  const rightIcon = <FontAwesomeIcon icon={faChevronRight} size="s" className="right-icon"/>
   return (
     <div className="quick-links-container">
       <div className="main-sub-header">
       <h3 className="main-sub-header-text">QUICK LINKS</h3>
     </div>
     <ul className="quick-links-ul">
-      <li className="quick-links-link">Air Quality Alert</li>
-      <li className="quick-links-link">Board of Trustees</li>
-      <li className="quick-links-link">Board Policies</li>
-      <li className="quick-links-link">Child Nutrition Website</li>
-      <li className="quick-links-link">Communications Help Desk</li>
-      <li className="quick-links-link">Curriculum Resources for Parents</li>
-      <li className="quick-links-link">Departments</li>
-      <li className="quick-links-link">Emergency Information</li>
-      <li className="quick-links-link">Find My School</li>
-      <li className="quick-links-link">Instructional Support Center -ISC</li>
-      <li className="quick-links-link">Local Control and Accountability PLan(LCAP)</li>
-      <li className="quick-links-link">Online Bidding for Warehouse Supplies</li>
-      <li className="quick-links-link">School Bell Schedules</li>
-      <li className="quick-links-link">School Messenger Notifications</li>
-      <li className="quick-links-link">School Year Calendars</li>
-      <li className="quick-links-link">Student of the Month</li>
-      <li className="quick-links-link">Superintendent's Message</li>
-      <li className="quick-links-link">TUSD Aeries Family Link</li>
-      <li className="quick-links-link">TUSD Farm </li>
-      <li className="quick-links-link">TUSD Mobile Tutoring</li>
+      <li className="quick-links-link">{rightIcon}Air Quality Alert</li>
+      <li className="quick-links-link">{rightIcon}Board of Trustees</li>
+      <li className="quick-links-link">{rightIcon}Board Policies</li>
+      <li className="quick-links-link">{rightIcon}Child Nutrition Website</li>
+      <li className="quick-links-link">{rightIcon}Communications Help Desk</li>
+      <li className="quick-links-link">{rightIcon}Curriculum Resources for Parents</li>
+      <li className="quick-links-link">{rightIcon}Departments</li>
+      <li className="quick-links-link">{rightIcon}Emergency Information</li>
+      <li className="quick-links-link">{rightIcon}Find My School</li>
+      <li className="quick-links-link">{rightIcon}Instructional Support Center -ISC</li>
+      <li className="quick-links-link">{rightIcon}Local Control and Accountability Plan</li>
+      <li className="quick-links-link">{rightIcon}Online Bidding for Warehouse Supplies</li>
+      <li className="quick-links-link">{rightIcon}School Bell Schedules</li>
+      <li className="quick-links-link">{rightIcon}School Messenger Notifications</li>
+      <li className="quick-links-link">{rightIcon}School Year Calendars</li>
+      <li className="quick-links-link">{rightIcon}Student of the Month</li>
+      <li className="quick-links-link">{rightIcon}Superintendent's Message</li>
+
     </ul>
   </div>
   );
--- src/components/StaffLinks.js
+++ src/components/StaffLinks.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const StaffLinks = props => {
+  const rightIcon = <FontAwesomeIcon icon={faChevronRight} size="s" className="right-icon"/>
   return (
     <div className="staff-links-container">
-      <p>Staff Links</p>
+      <div className="main-sub-header">
+        <h3 className="main-sub-header-text">STAFF LINKS</h3>
+      </div>
+      <ul className="staff-links-ul">
+        <li className="staff-links-link">{rightIcon}Aeries Staff Portal</li>
+        <li className="staff-links-link">{rightIcon}Clever Digital Programs</li>
+        <li className="staff-links-link">{rightIcon}Educational Technology</li>
+        <li className="staff-links-link">{rightIcon}Employee Health Benefits</li>
+        <li className="staff-links-link">{rightIcon}Negotiations Update</li>
+        <li className="staff-links-link">{rightIcon}Outlook Web Access for Staff</li>
+        <li className="staff-links-link">{rightIcon}Substitute Calling System</li>
+        <li className="staff-links-link">{rightIcon}Testing Calendar</li>
+      </ul>
     </div>
   );
 }
