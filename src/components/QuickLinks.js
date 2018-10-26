87a9986224dabf02ac7a42deebc84d6e9e47d08c
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-26 08:06:17 -0700

    adjusting quick links margin and formatting

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -3,8 +3,32 @@ import React from 'react';
 const QuickLinks = props => {
   return (
     <div className="quick-links-container">
-      <p>Quicklinks</p>
+      <div className="main-sub-header">
+      <h3 className="main-sub-header-text">QUICK LINKS</h3>
     </div>
+    <ul className="quick-links-ul">
+      <li className="quick-links-link">Air Quality Alert</li>
+      <li className="quick-links-link">Board of Trustees</li>
+      <li className="quick-links-link">Board Policies</li>
+      <li className="quick-links-link">Child Nutrition Website</li>
+      <li className="quick-links-link">Communications Help Desk</li>
+      <li className="quick-links-link">Curriculum Resources for Parents</li>
+      <li className="quick-links-link">Departments</li>
+      <li className="quick-links-link">Emergency Information</li>
+      <li className="quick-links-link">Find My School</li>
+      <li className="quick-links-link">Instructional Support Center -ISC</li>
+      <li className="quick-links-link">Local Control and Accountability PLan(LCAP)</li>
+      <li className="quick-links-link">Online Bidding for Warehouse Supplies</li>
+      <li className="quick-links-link">School Bell Schedules</li>
+      <li className="quick-links-link">School Messenger Notifications</li>
+      <li className="quick-links-link">School Year Calendars</li>
+      <li className="quick-links-link">Student of the Month</li>
+      <li className="quick-links-link">Superintendent's Message</li>
+      <li className="quick-links-link">TUSD Aeries Family Link</li>
+      <li className="quick-links-link">TUSD Farm </li>
+      <li className="quick-links-link">TUSD Mobile Tutoring</li>
+    </ul>
+  </div>
   );
 }
 export default QuickLinks;
