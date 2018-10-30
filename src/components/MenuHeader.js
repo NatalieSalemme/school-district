bf839216d500c31ac6b750bc6fcd1a00fc5b1bcf
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-30 11:42:30 -0700

    making navbar into a grid

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSchool, faFacebook, faTwitter, faInstagram, faLinkedIn } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSchool } from '@fortawesome/free-solid-svg-icons';
 
 const MenuHeader = () => {
   const schoolIcon = <FontAwesomeIcon icon={faSchool} size="7x" className="school-icon" />
-  const facebookIcon = <FontAwesomeIcon icon={faFacebook} size="2x" className="facebook-icon" />
-  const twitterIcon = <FontAwesomeIcon icon={faTwitter} size="2x" className="twitter-icon" />
-  const instagramIcon = <FontAwesomeIcon icon={faInstagram} size="2x" className="instagram-icon" />
-  const linkedInIcon = <FontAwesome icon={faLinkedIn} size="2x" className="linked-in-icon" />
+  // const facebookIcon = <FontAwesomeIcon icon={fabFacebookF} size="2x" className="facebook-icon" />
+  // const twitterIcon = <FontAwesomeIcon icon={faTwitter} size="2x" className="twitter-icon" />
+  // const instagramIcon = <FontAwesomeIcon icon={faInstagram} size="2x" className="instagram-icon" />
+  // const linkedInIcon = <FontAwesome icon={faLinkedIn} size="2x" className="linked-in-icon" />
 
   return (
     <div className="menu-header">
@@ -28,8 +28,21 @@ const MenuHeader = () => {
     <div className="menu-header-second-col">
       <p>nav bar</p>
       <div className="social-media-icons">
-
+        <span className="fa fa-facebook"></span>
+        <i className="fab fa-facebook-f" style={{color: 'white'}}></i>
+        {/* <span>{facebookIcon}</span> */}
+        {/* <span>{twitterIcon}</span>
+        <span>{instagramIcon}</span>
+        <span>{linkedInIcon}</span> */}
       </div>
+        <ul className="nav-bar-links">
+          <li>HOME</li>
+          <li>INFO</li>
+          <li>DISTRICT</li>
+          <li id="student-services">STUDENT SERVICES</li>
+          <li>SCHOOL</li>
+          <li>RESOURCES</li>
+      </ul>
     </div>
     </div>
   );
