c2c8c19ce9ec5ac9c14ce80f75297bb294b619ba
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-30 18:56:02 -0700

    Changing background color and adjusting padding on about us card

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,8 +9,10 @@ const AboutUs = () => {
       <img
       src={require('../images/sd-school.jpg')}
       alt="San Diego School District Building" />
+      <div className="about-us-text">
       <h4 className="secondary-header">ABOUT US</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   );
 }
